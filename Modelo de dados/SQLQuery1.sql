ed48d7b3c12b861e5ed79b427fd70ef2847d97b0
Daniel Souza <66451755+DanielSouzaBarros@users.noreply.github.com>
2023-11-28 11:33:27 -0300

    Add files via upload

---
+++ Modelo de dados/SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿use test_SQL
+
+--Valor total das vendas e dos fretes por produto e ordem de venda;
+--● Valor de venda por tipo de produto;
+--● Quantidade e valor das vendas por dia, mês, ano;
+--● Lucro dos meses;
+--● Venda por produto;
+--● Venda por cliente, cidade do cliente e estado;
+--● Média de produtos vendidos;
+--● Média de compras que um cliente faz.
+ SELECT 
+   p.Produto,
+   fd.CupomID,
+   SUM(fd.Valor) AS TotalVendas,
+   SUM(fd.ValorFrete) AS TotalFretes
+FROM FatoDetalhes fd
+JOIN Produtos p ON fd.ProdutoID = p.ProdutoID
+GROUP BY p.Produto, fd.CupomID;
+
